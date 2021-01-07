7805f70893187408dfe9c123d86e4609fb9c40cb
Maung Naing <mpnaing@ucdavis.edu>
2021-01-07 22:04:22 +0000

    trying to modify home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get  'home_page/home'
   root 'home_page#home'
 end
--- test/controllers/home_page_controller_test.rb
+++ test/controllers/home_page_controller_test.rb
@@ -4,5 +4,6 @@ class HomePageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_page_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 end
