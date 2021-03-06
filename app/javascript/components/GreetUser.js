878d1b59dc4251b91b7a5dc084f8c5efd20315b9
Maung Naing <mpnaing@ucdavis.edu>
2021-03-06 01:04:32 +0000

    home page frontend react look

--- app/javascript/components/GreetUser.js
+++ app/javascript/components/GreetUser.js
@@ -5,22 +5,27 @@ import axios from 'axios'
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
+import './GreetUser.css'
 
 class GreetUser extends React.Component {
   render () {
     return (
+    <div className="home__body">
+      <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt="logo"
+        />
      <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
         <input />
-        <MicIcon />
       </div>
 
       <div className="search__buttons">
-        <Button variant="outlined" type="submit">Google Search</Button>
-        <Button variant="outlined">I'm Feeling Lucky</Button>
+        <Button variant="outlined" type="submit">Doogle Search</Button>
       </div>
     </form>
+    </div>
     );
   }
 }
