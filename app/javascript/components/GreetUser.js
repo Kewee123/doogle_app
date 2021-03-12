d9f8e21f2313000c9d59cf98cb026c7e2f042944
Maung Naing <mpnaing@ucdavis.edu>
2021-03-12 23:56:37 +0000

    connected the frontend to backend via axios get

--- app/javascript/components/GreetUser.js
+++ app/javascript/components/GreetUser.js
@@ -7,36 +7,56 @@ import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
 
 class GreetUser extends React.Component {
-  state = {
-    searchString: '',
+  constructor(props){
+    super(props)
+    this.state = {
+      searchString: '',
+      returnedData: '',
+    };
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({
+      searchString: event.target.value
+    });
   }
   
   handleSubmit = event => {
+    console.log('A name was submitted: ' + this.state.searchString);
     event.preventDefault();
+    const searchStr = this.state.searchString;
+    const self = this;
 
-    const user = {
-      name: this.state.name
-    };
-
-    axios.post(`/api/words`, { user })
+    axios.get(`/api/words`, { params:{id: searchStr} })
       .then(res => {
-        console.log(res);
-        console.log(res.data);
-      })
+        console.log(res.status);
+        
+        if(typeof res.data == 'object'){
+          console.log(res.data[0]);
+          self.setState({returnedData: res.data[0].content});
+          console.log('hello');
+        } else {
+          self.setState({returnedData: res.data});
+        }
+    })
   }
-
   
   render () {
+    console.log(this.state);
     return (
     <div className="home__body">
       <img
           src={"/assets/doogleImage.png"}
           alt="logo"
         />
+      {this.state.returnedData ? <div> {this.state.returnedData} </div> : null} 
      <form className="search" onSubmit={this.handleSubmit}>
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input />
+        <input type="text" value={this.state.searchString} onChange={this.handleChange}/>
       </div>
 
       <div className="search__buttons">
